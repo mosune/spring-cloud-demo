eefc04a62f5dc56c224e7a94ef8e4dbda020acb0
gcg <526409252@qq.com>
2018-01-23 10:22:39 +0800

    day2

--- service-ribbon/src/main/java/com/cloud/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/cloud/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.cloud.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,8 +19,13 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError") // 熔断器注解，并设置熔断方法为hiError
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-CLIENT/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 
 }
